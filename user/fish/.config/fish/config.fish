7cc5965a02f272ab88b928c4d9214610026409b3
Filinto Delgado <filintodelgado@gmail.com>
2024-12-19 16:43:29 +0000

    Fish: Change prompt from starship to tide

--- user/fish/.config/fish/config.fish
+++ user/fish/.config/fish/config.fish
@@ -1,6 +1,11 @@
 set -g fish_greeting
 
 if status is-interactive
-    starship init fish | source
     fzf --fish | source
+end
+
+function add_new_line_after_command --on-event fish_postexec
+    if ! test "$argv" = "clear"
+        echo
+    end
 end
