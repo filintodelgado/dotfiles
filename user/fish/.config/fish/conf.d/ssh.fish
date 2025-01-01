7de05b4ad048140671928626af968713413a7448
Filinto Delgado <filintodelgado@gmail.com>
2025-01-01 23:23:06 +0000

    Fish: Only start one ssh-agent per user
    Previoully each new fish session would create a new ssh-agent process
    that would never be terminated.
    
    As a fix the ssh-agent will be reused instead.

--- user/fish/.config/fish/conf.d/ssh.fish
+++ user/fish/.config/fish/conf.d/ssh.fish
@@ -1,10 +1,15 @@
-# open connection to the ssh agent
-eval (ssh-agent -c) > /dev/null
-
-# add the default keys
-ssh-add &> /dev/null
-
-# add the custom keys
-for key in (find ~/.ssh/keys -type f | grep -v '^.*\.pub')
-  ssh-add $key &> /dev/null
+if ! pgrep -u "$USER" ssh-agent > /dev/null
+  ssh-agent -t 1h -c > "$XDG_RUNTIME_DIR/ssh-agent.env"
 end
+
+if ! test -f "$SSH_AUTH_SOCK"
+  source "$XDG_RUNTIME_DIR/ssh-agent.env" &> /dev/null
+  # add the default keys
+  ssh-add &> /dev/null
+
+  # add the custom keys
+  for key in (find ~/.ssh/keys -type f | grep -v '^.*\.pub')
+    ssh-add $key &> /dev/null
+  end
+end
+
