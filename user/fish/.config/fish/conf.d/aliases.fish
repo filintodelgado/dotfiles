be7a504c2db210c4681047c0bb33cfad50d9a3ff
Filinto Delgado <filintodelgado@gmail.com>
2025-01-19 19:12:59 +0000

    Fish: Remove "ld" alias
    'eza' alias 'ld' was overriding ld (linker) command

--- user/fish/.config/fish/conf.d/aliases.fish
+++ user/fish/.config/fish/conf.d/aliases.fish
@@ -2,7 +2,6 @@
 alias l='eza -lh  --icons=auto' # long list
 alias ls='eza -1   --icons=auto' # short list
 alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
-alias ld='eza -lhD --icons=auto' # long list dirs
 alias lt='eza --icons=auto --tree' # list folder as tree
 
 # Handy change dir shortcuts
