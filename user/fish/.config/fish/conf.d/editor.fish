f8409ca21964d49594d351ce203fe2e7dcc0e649
Filinto Delgado <filintodelgado@gmail.com>
2025-01-26 21:23:18 +0000

    Fish: Make bat the systemd manpager

--- user/fish/.config/fish/conf.d/editor.fish
+++ user/fish/.config/fish/conf.d/editor.fish
@@ -1,6 +1,15 @@
+set -x BROWSER  'firefox'
+
 set -x EDITOR   'lvim'
-set -x MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
+alias vim 'lvim'
+
 set -x PAGER    'bat -p'
+set -x MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -l man --paging=always'"
+
+# journalctl and systemctl output
+set -x SYSTEMD_PAGER "bat -p -l syslog"
+set -x SYSTEMD_COLORS false
+set -x SYSTEMD_PAGERSECURE true
 
 alias less 'bat -p'
 alias cat 'bat'
