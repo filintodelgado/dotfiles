91d04c98e794081ea6a90485aed08e96cf06eef4
Filinto Delgado <filintodelgado@gmail.com>
2025-11-21 00:23:24 +0000

    Fish: New keybinding to open EDITOR
    This can also bring EDITOR to foreground

--- user/fish/.config/fish/conf.d/editor.fish
+++ user/fish/.config/fish/conf.d/editor.fish
@@ -12,3 +12,22 @@ set -x SYSTEMD_PAGERSECURE true
 
 alias less 'bat -p'
 alias cat 'bat'
+
+function edit --description "Edit a file or on the current directory with the default \$EDITOR" -a file --wraps "$EDITOR"
+    # Bring editor to foreground
+    set job (jobs | grep "$EDITOR")
+
+    if test -n "$job"
+        set job_id (echo $job | cut -f1 )
+        fg (jobs -p %"$job_id")
+        return
+    end
+
+    # Open a new editor with the file or in the current dir
+    if test -z "$file"
+        set file .
+    end
+    $EDITOR $file
+end
+
+bind -M insert ctrl-e edit
