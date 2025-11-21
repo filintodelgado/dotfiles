ce2221a6e3fc6af3f3c1dd8509e7e09b21cdc032
Filinto Delgado <filintodelgado@gmail.com>
2025-11-21 00:36:10 +0000

    NeoVim: Update LSP to new configuration type
    Now you just need to enable the LSPs

--- user/nvim/.config/nvim/lua/plugins/bar.lua
+++ user/nvim/.config/nvim/lua/plugins/bar.lua
@@ -1,5 +1,3 @@
--- lualine.nvim
-
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' }
--- user/nvim/.config/nvim/lua/plugins/lsp.lua
+++ user/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,55 +2,11 @@ return {
     "neovim/nvim-lspconfig",
     event = "BufReadPre",
     config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        local lspconfig = require("lspconfig")
-
-        -- Python
-        lspconfig.pylsp.setup({
-            capabilities = capabilities,
-        })
-
-        -- TypeScript/JavaScript
-        lspconfig.ts_ls.setup({
-            capabilities = capabilities,
-        })
-
-        -- Go
-        lspconfig.gopls.setup({
-            capabilities = capabilities,
-        })
-
-        -- Rust
-        lspconfig.rust_analyzer.setup({
-            capabilities = capabilities,
-        })
-
-        -- C/C++
-        lspconfig.clangd.setup({
-            capabilities = capabilities,
-            cmd = { "clangd", "--background-index" },
-            filetypes = { "c", "cpp" },
-        })
-
-        -- Lua
-        lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-                Lua = {
-                     runtime = {
-                         version = 'LuaJIT',
-                     },
-                    diagnostics = {
-                        globals = {'vim'},
-                    },
-                    workspace = {
-                        library = vim.api.nvim_get_runtime_file("", true),
-                    },
-                    telemetry = {
-                        enable = false,
-                    },
-                },
-            },
-        })
+        vim.lsp.enable('clangd')
+        vim.lsp.enable('ts_ls')
+        vim.lsp.enable('gopls')
+        vim.lsp.enable('rust_analyzer')
+        vim.lsp.enable('lua_ls')
+        vim.lsp.enable('pyright')
     end
 }
