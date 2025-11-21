411775af6e28af3729e9e339126f7b0c254132ec
Filinto Delgado <filintodelgado@gmail.com>
2025-11-21 00:34:43 +0000

    NeoVim: Disable relative number

--- user/nvim/.config/nvim/lua/options.lua
+++ user/nvim/.config/nvim/lua/options.lua
@@ -1,25 +1,32 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- enable 24-bit colour
+-- enable 24-bit color
 vim.opt.termguicolors = true
-vim.cmd 'colorscheme catppuccin-mocha'
-vim.cmd 'set relativenumber'
+vim.opt.relativenumber = false
+
+-- color scheme
+vim.cmd.colorscheme "catppuccin-mocha"
 
 -- fully disables the mouse
 vim.opt.cursorline = false
 vim.opt.mouse = ""
 
 -- use spaces to separate
-vim.o.expandtab = true
+vim.opt.expandtab = true
 -- automatically indent
-vim.o.smartindent = true
+vim.opt.smartindent = true
 -- show tabs as 4 spaces
-vim.o.tabstop = 4
+vim.opt.tabstop = 4
 -- indent always with 4 spaces
-vim.o.shiftwidth = 4
+vim.opt.shiftwidth = 4
+-- show line number
+vim.opt.number = true
 
 -- show hidden characters
-vim.o.list = true
-vim.o.listchars = "tab:→ ,space:·,trail:·,nbsp:·,extends:>,precedes:<"
+vim.opt.list = true
+vim.opt.listchars = "tab:→ ,space:·,trail:·,nbsp:·,extends:>,precedes:<,eol:↴"
+
+-- spellchecking
+vim.opt.spell = true
 
