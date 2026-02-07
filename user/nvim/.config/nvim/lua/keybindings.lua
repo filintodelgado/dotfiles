50918442bba9e44467195f3eb5f290707b3f9b63
Filinto Delgado <filintodelgado@gmail.com>
2026-02-07 14:23:36 +0000

    NeoVim: Add remaining tab management keybinding

--- user/nvim/.config/nvim/lua/keybindings.lua
+++ user/nvim/.config/nvim/lua/keybindings.lua
@@ -6,22 +6,42 @@ vim.keymap.set('n', '<A-l>', ':NvimTreeClose<CR>', { noremap = true, silent = tr
 local builtin = require('telescope.builtin')
 function find_files()
     builtin.find_files({
-        hidden=true
+        hidden = true
     })
 end
+
 function live_grep()
     builtin.live_grep({
         additional_args = { "--hidden" },
     })
 end
-vim.keymap.set('n', '<C-p>', ':Telescope<CR>', { desc = 'Telescope' })
-vim.keymap.set('n', '<C-o>', find_files, { desc = 'Telescope find files' })
+
+vim.keymap.set('n', '<A-p>', ':Telescope<CR>', { desc = 'Telescope', nowait = true, silent = true })
+vim.keymap.set('n', '<C-p>', find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-f>', live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<A-o>', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<C-o>', builtin.buffers, { desc = 'Telescope buffers' })
 
 -- Neogit
 vim.keymap.set('n', '<leader>gg', '<cmd>Neogit<cr>', { desc = 'Show Neogit UI' })
 
 -- LSP
-vim.keymap.set('n', '<A-enter>', vim.diagnostic.open_float, { desc = 'Show diagnostic' })
-vim.keymap.set('n', '<S-CR>', vim.lsp.buf.type_definition, { desc = 'Goto type definition' })
+vim.keymap.set('n', '<C-CR>', vim.lsp.buf.hover, { desc = 'Show symbol documentation' })
+vim.keymap.set('n', '<A-CR>', vim.diagnostic.open_float, { desc = 'Show diagnostic' })
+vim.keymap.set('n', '<CR>', vim.lsp.buf.type_definition, { desc = 'Goto type definition' })
+vim.keymap.set('n', '<C-.>', vim.lsp.buf.code_action, { desc = 'Suggest fix' })
+vim.keymap.set('n', '<S-CR>', vim.lsp.buf.rename, { desc = 'Rename symbol' })
+
+-- Tabs
+vim.keymap.set('n', '<C-t>', ':tabnew<CR>', { desc = "Open new tab", silent = true })
+
+vim.keymap.set('n', '<S-j>', ':-tabnext<CR>', { desc = "Goto previous tab", silent = true })
+vim.keymap.set('n', '<S-k>', ':+tabnext<CR>', { desc = "Goto next tab", silent = true })
+
+vim.keymap.set('n', '<leader>t0', ':tabnext 1<CR>', { desc = "Goto first tab", silent = true })
+vim.keymap.set('n', '<leader>t$', ':tabnext $<CR>', { desc = "Goto last tab", silent = true })
+vim.keymap.set('n', '<C-w>', ':tabclose<CR>',
+    { desc = "Goto previous accessed tab", nowait = true, silent = true, noremap = true })
+
+-- Navigation
+vim.keymap.set('n', 'H', '<C-o>', { desc = "Jump to previous location" })
+vim.keymap.set('n', 'L', '<C-i>', { desc = "Jump to next location" })
